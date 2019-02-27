0606d25bf86417214edf59d15d002910ee9001a1
Andreas Julius <ajul@mac.com>
2019-02-27 18:38:06 +0100

    added new rules for crafting Splendid Boots correct add_item for inventory handling

--- add_item.sh
+++ add_item.sh
@@ -1,19 +1,18 @@
 # associatve arrays in bash > 4.2
 
-declare -A sum inv
+declare -A sum
+
+[ $# -eq 1 ] &&
+while read cnt type item
+do
+	(( sum[$type/$item] -= cnt ))
+done <$1
 
 # build array with the sum of all item counts from stdin
 while read pre type item cnt
 do
 	(( sum[$type/$item] += cnt ))
 done
-
-[ $# -eq 1 ] &&
-while read pre type item cnt
-do
-	(( sum[$type/$item] > 0 )) &&
-		(( sum[$type/$item] -= cnt ))
-done <$1
 
 for i in ${!sum[@]}	# all items
 do
